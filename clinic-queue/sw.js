3cec303a6a7b034acc6a5d662d17ede49f035088
orameshkumar <59666060+orameshkumar@users.noreply.github.com>
2026-07-01 16:31:53 +0530

    issue fix

--- clinic-queue/sw.js
+++ clinic-queue/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "clinic-queue-v49";
+const CACHE_NAME = "clinic-queue-v50";
 const SHELL_FILES = [
   "./index.html", "./reception.html", "./doctor.html", "./board.html",
   "./self-service.html", "./admin.html", "./patients.html", "./billing.html",
