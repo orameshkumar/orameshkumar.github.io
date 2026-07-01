4aba718bcfb9a68e2ba9c0d82a8cdd860bf60c64
orameshkumar <59666060+orameshkumar@users.noreply.github.com>
2026-07-01 06:27:35 +0530

    bug fix

--- clinic-queue/sw.js
+++ clinic-queue/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "clinic-queue-v16";
+const CACHE_NAME = "clinic-queue-v18";
 const SHELL_FILES = [
   "./index.html", "./reception.html", "./doctor.html", "./board.html",
   "./self-service.html", "./admin.html", "./patients.html", "./billing.html",
