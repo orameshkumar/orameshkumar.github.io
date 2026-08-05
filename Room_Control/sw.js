1ac4d6d8d511869e212de9394044fed1039702d3
orameshkumar <59666060+orameshkumar@users.noreply.github.com>
2026-08-05 22:38:31 +0530

    issue fix

--- Room_Control/sw.js
+++ Room_Control/sw.js
@@ -1,15 +1,25 @@
-const CACHE = 'roomctrl-fb-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
+const CACHE = 'roomctrl-fb-v2';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
   self.clients.claim();
 });
 
